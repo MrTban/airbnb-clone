bdd37098b26a87bb91bcd8d284f407d909ff9a4a
MrTban <esteman214@gmail.com>
2023-04-29 20:14:26 -0500

    RentModal

--- app/components/Modals/RegisterModal.tsx
+++ app/components/Modals/RegisterModal.tsx
@@ -6,16 +6,19 @@ import { FcGoogle } from 'react-icons/fc'
 import { useCallback, useState } from 'react'
 import { FieldValues, SubmitHandler, useForm } from 'react-hook-form'
 
-import userRegisterModal from '@/app/hooks/useRegisterModal'
+import useRegisterModal from '@/app/hooks/useRegisterModal'
 import Modal from './Modal'
 import Heading from '../Heading'
 import Input from '../Inputs/Input'
 import { toast } from 'react-hot-toast'
 import Button from '../Button'
 import { signIn } from 'next-auth/react'
+import useLoginModal from '@/app/hooks/useLoginModal'
 
 const RegisterModal = () => {
-	const registerModal = userRegisterModal()
+	const registerModal = useRegisterModal()
+	const loginModal = useLoginModal()
+
 	const [isLoading, setIsLoading] = useState(false)
 
 	const {
@@ -45,6 +48,11 @@ const RegisterModal = () => {
 				setIsLoading(false)
 			})
 	}
+
+	const toggle = useCallback(() => {
+		registerModal.onClose()
+		loginModal.onOpen()
+	}, [loginModal, registerModal])
 
 	const bodyContent = (
 		<div className='flex flex-col gap-4'>
@@ -96,7 +104,7 @@ const RegisterModal = () => {
 				<div className='flex flex-row items-center gap-2'>
 					<div>Already have an account?</div>
 					<div
-						onClick={registerModal.onClose}
+						onClick={toggle}
 						className=' text-neutral-800 cursor-pointer hover:underline'
 					>
 						Log in
--- app/components/Navbar/UserMenu.tsx
+++ app/components/Navbar/UserMenu.tsx
@@ -3,10 +3,13 @@
 import { AiOutlineMenu } from 'react-icons/ai'
 import Avatar from '../Avatar'
 import { useCallback, useState } from 'react'
-import MenuItem from './MenuItem'
-import userRegisterModal from '@/app/hooks/useRegisterModal'
-import useLoginModal from '@/app/hooks/useLoginModal'
 import { signOut } from 'next-auth/react'
+
+import MenuItem from './MenuItem'
+
+import useRegisterModal from '@/app/hooks/useRegisterModal'
+import useLoginModal from '@/app/hooks/useLoginModal'
+import useRentModal from '@/app/hooks/useRentModal'
 import { SafeUser } from '@/app/types'
 
 interface UserMenuProps {
@@ -14,19 +17,29 @@ interface UserMenuProps {
 }
 
 const UserMenu: React.FC<UserMenuProps> = ({ currentUser }) => {
-	const registerModal = userRegisterModal()
+	const registerModal = useRegisterModal()
 	const loginModal = useLoginModal()
+	const rentModal = useRentModal()
+
 	const [isOpen, setIsOpen] = useState(false)
 
 	const toggleOpen = useCallback(() => {
 		setIsOpen((value) => !value)
 	}, [])
 
+	const onRent = useCallback(() => {
+		if (!currentUser) {
+			return loginModal.onOpen()
+		}
+
+		rentModal.onOpen()
+	}, [currentUser, loginModal, rentModal])
+
 	return (
 		<div className='relative'>
 			<div className='flex flex-row items-center gap-3'>
 				<div
-					onClick={() => {}}
+					onClick={onRent}
 					className=' hidden md:block text-sm font-semibold py-3 px-4 rounded-ful  hover:bg-neutral-100 transition cursor-pointer'
 				>
 					Airbnb your home
@@ -51,7 +64,7 @@ const UserMenu: React.FC<UserMenuProps> = ({ currentUser }) => {
 								<MenuItem onClick={() => {}} label='My favorites' />
 								<MenuItem onClick={() => {}} label='My reservations' />
 								<MenuItem onClick={() => {}} label='My properties' />
-								<MenuItem onClick={() => {}} label='Airbnb my home' />
+								<MenuItem onClick={rentModal.onOpen} label='Airbnb my home' />
 								<hr />
 								<MenuItem onClick={() => signOut()} label='Logout' />
 							</>
